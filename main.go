49461ba2fbd0a4dd6961e0a8477672978182414f
Tesla <anuar.musljumov@gmail.com>
2024-09-02 11:11:41 +0500

    Third commit with some fun function

--- main.go
+++ main.go
@@ -6,10 +6,20 @@ func hello(msg string) {
 	fmt.Println("Hello,", msg)
 }
 
+func infinityLoop() {
+	i := 0
+	for {
+		i++
+	}
+}
+
 func main() {
 	hello("Vanya")
 	hello("Andrey")
 	hello("Alexey")
 	hello("Maxim")
 	hello("Vasya")
+
+	// for fun
+	infinityLoop()
 }
